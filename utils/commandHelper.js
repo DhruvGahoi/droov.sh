cd5f65add52c13e6e10fb4a9fe23d0d0b9d078f2
DhruvGahoi <dhruvgahoi11111@gmail.com>
2025-09-14 14:52:51 +0530

    updated Resume

--- utils/commandHelper.js
+++ utils/commandHelper.js
@@ -160,7 +160,7 @@ export const CONTENTS = {
 
   resume: () => {
     window.open(
-      "https://drive.google.com/file/d/1XSDh5Dsn0aUhdo2d4Anc35n-aqsGSE7Q/view?usp=sharing",
+      "https://drive.google.com/file/d/1ZvDfTF4UW32CxsN2w7AUa2EV2_pobpXR/view?usp=drive_link",
       "_blank",
     );
     return "Opening resume in a new tab...";
